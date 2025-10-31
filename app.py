3990537546288d9230c7ed0f6700509585d90c4b
MindFrameAii <aathiaty@gmail.com>
2025-10-31 19:08:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,6 @@ def download_log(filename):
     return send_from_directory('logs', filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8501, debug=True)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)   
